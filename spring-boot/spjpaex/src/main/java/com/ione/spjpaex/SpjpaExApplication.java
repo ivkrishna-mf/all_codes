bf8e07ad373dd9cf8eb25372d5cd611a518c2cca
ikrishna <krishna@ione.com>
2024-01-15 14:42:22 +0530

    initial project rbse

---
+++ spring-boot/spjpaex/src/main/java/com/ione/spjpaex/SpjpaExApplication.java
@@ -0,0 +1,13 @@
+package com.ione.spjpaex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpjpaExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpjpaExApplication.class, args);
+	}
+
+}
